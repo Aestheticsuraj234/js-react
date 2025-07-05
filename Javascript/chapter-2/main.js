de2fefe54970ff9288516d65de21a3ff5eeb342a
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-05 10:54:39 +0530

    chapter-2

---
+++ Javascript/chapter-2/main.js
@@ -0,0 +1,92 @@
+// Operators : 
+
+
+console.log(typeof undefined);
+
+// *4. Logical Operators ( && (AND) , || (OR) , ! (NOT) )
+
+let num1 = 20;
+let num2 = 20;
+
+
+// console.log(num1 === num2 && num1 < num2);
+
+// console.log(true && true);
+// console.log(false && true);
+// console.log(true && false);
+// console.log(false && false);
+
+
+// console.log(true || true);
+// console.log(false || true);
+// console.log(true || false);
+// console.log(false || false);
+
+
+console.log(!true);
+console.log(!false);
+
+
+
+// *3. Comparison Operators ( == , === , != , !== , > , < , >= , <= )
+
+
+// let value1 = 15;
+// let value2 = 15;
+
+// // console.log(value1 == value2); // value
+// // console.log(value1 === value2); // value , data type
+// // console.log(value1 != value2); //true , false
+// // console.log(value1 !== value2);
+
+
+// // console.log(value1 > value2);
+// // console.log(value1 < value2);
+// console.log(value1 >= value2);
+// console.log(value1 <= value2);
+
+
+
+
+
+
+
+
+
+
+// *2. Assignment Operators ( = , += , -= , *= , /= , %= , **= )
+
+// let num1 = 44;
+
+// // num1 += 2; // num1 = 44 + 2; = 46;
+// num1 *= 2;
+// console.log(num1);
+
+
+
+
+
+
+
+
+
+
+//* 1. Arithmetic Operators ( + , - , * , / , % , ** , ++ , --)
+
+
+//* 1. Post increment/decrement
+// console.log(a--)
+// console.log(a)
+//* 2. Pre increment/decrement
+// console.log(++b)
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// console.log(a++);
+// console.log(a);
+// console.log(a--);
+// console.log(a);
+
