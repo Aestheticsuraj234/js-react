15b62b74cfc61485cfc636da12f571a9ac212638
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-07 13:00:01 +0530

    chapter-3

---
+++ Javascript/chapter-3/loop.js
@@ -0,0 +1,47 @@
+// 1-10;
+// console.log(1)
+// console.log(2)
+
+
+
+
+// console.log(10)
+
+
+// 1-100;
+
+// console.log(1)
+
+
+// console.log(100)
+
+
+// 1-1000;
+// "Good Morning";
+// console.log(1)
+// console.log("good morning")
+
+// for , while , do-while
+
+// for(intialisation; condition ; increment/decrement){
+//     //code to execute
+// }
+
+// for(let i = 1; i<=15; i++){
+//     console.log("Hello")
+// }
+// // 1
+// // 2
+// // 3
+// // 4
+// let i = 1;
+// while(i<=5){
+// console.log("Hello");
+// i++
+// }
+
+// let num = 1;
+// do{
+// console.log(num);
+// num++
+// }while(num<=5);
---
+++ Javascript/chapter-3/main.js
@@ -0,0 +1,32 @@
+// let age = 27;
+
+// if(age ===20){
+// console.log("Hello Suraj");
+// }
+// else if(age === 19){
+// console.log("Your age is equal to 19")
+// }
+// else if(age <18){
+//     console.log("hello you age is less then 18")
+// }
+// else{
+//     console.log("Hello karan")
+// }
+
+// let grade = "B";
+
+// switch (grade) {
+//   case "A":
+//     console.log("Excellent");
+//   case "B":
+//     console.log("Good");
+//     break;
+//   case "C":
+//     console.log("Average");
+//   default:
+//     console.log("Try again");
+// }
+
+//* 3. Ternary Operator (? :)
+// let age = 18;
+// age >= 18 ? console.log("Age is 18") : console.log("age is not 18");
