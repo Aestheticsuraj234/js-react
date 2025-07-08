b5ab57178bd8ccb006331a4f3584da094f3bf8fb
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-08 10:31:06 +0530

    chapter-4

---
+++ Javascript/chapter-4/main.js
@@ -0,0 +1,59 @@
+// function greet(name) {
+//   return `Hello, ${name}!`;
+// }
+
+// console.log(greet("Alice")); // Hello, Alice!
+
+// // const greet = function(){
+// //     console.log("Hello world!")
+// // }
+// // greet()
+
+//* Paramter and Argument
+// Paramter ---> Function decleration 
+// Argument ----> Function calling 
+
+// function greetUsername(username , age){
+// console.log(`Hello👋🏻! Good Morning🌄` , username , age)
+// }
+
+
+// // greetUsername("Suraj")
+// greetUsername("Karan" , 21)
+
+
+// *return Keyword
+
+function add(num1 , num2){
+    return num1 + num2;
+}
+
+console.log(add(14 , 15))
+
+
+// *1. function decleration
+// *2. function calling
+// *3. paramters and arguments
+// *4. return keyword
+// *5. function expression
+// *6. arrow function
+// *7. annonymous function
+// *8. IIFE (Immediately Invoked Function Expression)
+
+
+
+// const Sub = num1 =>console.log(num1-10);
+
+// Sub(20)
+
+
+
+// let suraj = function(){
+//     // code to execute
+// }
+
+// suraj();
+
+(function(){
+    console.log("Hello world")
+})()
