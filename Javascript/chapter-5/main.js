b5a2e8ddab84d32a123e8ed003b9bd02a97971d5
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-10 11:58:08 +0530

    chapter-5

---
+++ Javascript/chapter-5/main.js
@@ -0,0 +1,31 @@
+
+
+// let fruits = new Array("🍍" , "🍎" , "🍇" , "🍈");
+
+// let fruits = [];
+
+// console.log(fruits)
+
+
+// let fruits = ["🍍" , "🍎" , "🍇" , "🍈"];
+
+// console.log(fruits[5])
+
+
+// let cars = ["🚗" , "🚕" , "🚙" , "🚌" , "🚚"];
+
+// let food = ["🍔" , "🍕" , "🍟" , "🍔" , "🍕" , "🍟"];
+
+// console.log(food[0])
+// console.log(food[food.length - 1])
+
+
+// let signEmojis = ["🚨" , , "⚠️" ];
+// console.log(signEmojis[1])
+
+// let mixed = [1, 'hello', true, null, [5, 6]];
+
+
+let multiarray = [1,2,3,4,5,[6,7,8,[9,10]]];
+
+console.log(multiarray[5][3][0])
---
+++ Javascript/chapter-5/methods.js
@@ -0,0 +1,94 @@
+// const arr = [1,23,4,5,6];
+
+// arr.push(10);
+// console.log(arr)
+
+// const arr = [12,3,56,7];
+// let res = arr.pop();
+// console.log(arr , res)
+
+// * Splice method
+// 1, remove
+// const arr1 = [1,2,3,4];
+// arr1.splice(0 , 2);
+
+// console.log(arr1)
+
+// 2. replace
+
+// const arr2 = [1,2,3,4];
+// arr2.splice(0,2,"Suraj" , "Karan");
+// console.log(arr2)
+
+// 3. add elements
+
+// const arr3 = [1,2,3];
+// arr3.splice(1 , 0 , "hello" , "world");
+// console.log(arr3)
+
+// const arr4 = [1, 2, 3, 4, 5];
+// arr4.splice(2); // removes from index 2 to end
+// console.log(arr4); // [1, 2]
+
+// const arr = [3,1,2,0];
+
+// arr.sort((a,b)=>b-a);
+
+// console.log(arr)
+
+// const arr = [143,2,3,14,5];
+
+// console.log(arr.reverse())
+
+// * Slice method(start , end);
+
+// const arr = [1,2,3,4,5];
+
+// console.log(arr.slice(0 , 4))
+
+// console.log(arr)
+
+// const a = [1], b = [2];
+// const result = a.concat(b); // [1, 2]
+
+// console.log(result)
+
+// let food = ["🍔" , "🍕" , "🍟" , "🍔" , "🍕" , "🍟"];
+
+// console.log(food.indexOf("🔥"))
+
+// let tags = ["coding" , "javascript" , "py"];
+
+// console.log(tags.join("12323"))
+
+// For each method;
+
+// let array = [1, 2, 3, 4, 5];
+
+// const total = array.reduce((acc, cur) => {
+//   return acc + cur
+// }, 0);
+
+// // 1 
+// // 3
+// // 6
+// // 10
+// // 15
+// console.log(total);
+
+//  console.log(array.filter((value , index , array)=>{
+//     return  value > 4
+//  }))
+
+// let res = array.map((elem , index , arr)=>{
+//     return elem * 3
+// })
+
+// console.log(res)
+// console.log(array)
+
+// let res = array.forEach((currentElement , index , array)=>{
+//    return 2 * currentElement
+// })
+
+// console.log(res)
