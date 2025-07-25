1581452ceaec7a2a195a88ccd6388504d81c811f
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-25 12:35:25 +0530

    useState

--- react-js/my-app/src/App.jsx
+++ react-js/my-app/src/App.jsx
@@ -1,26 +1,27 @@
-import Card from "./Card";
+import {useState} from 'react'
 
 
+const App = () => {
+const [count , setCount] = useState(0);
+const [color , setColor] = useState("red");
 
-function App(){
-    return (
-        <>
-        
-        <Card
-        imageSrc="https://media.daily.dev/image/upload/s--xFBIY-LS--/f_auto/v1753347279/posts/JIo1qY5F3?_a=BAMClqZW0"
-        title="Github Spark is insane"
-        desc="GitHub Spark helps you transform your ideas into full-stack intelligent apps and publish with a single click."
-        />
+  return (
+    <div>
 
-          <Card
-        imageSrc="https://media.daily.dev/image/upload/f_auto,q_auto/v1/posts/4f23427091604f46dd498e291f24ac67?_a=AQAEulh"
-        title="Github Spark is insaaskdaso;kde"
-        desc="GitHub Spark helps you transform your ideas into full-stack intelligent apps and publish with a sinasdsadsadgle click."
-        />
-       
-       
-        </>
-    )
+<h1>My fav color is {color}</h1>
+<button onClick={()=>setColor("green")}>
+  Change Color
+</button>
+
+      <button onClick={()=>setCount(count+1)}>
+        +
+      </button>
+      {count}
+      <button onClick={()=>setCount(count-1)}>
+        -
+      </button>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ react-js/my-app/src/notes.md
@@ -0,0 +1,21 @@
+import Card from "./Card";
+
+function App() {
+  return (
+    <>
+      <Card
+        imageSrc="https://media.daily.dev/image/upload/s--xFBIY-LS--/f_auto/v1753347279/posts/JIo1qY5F3?_a=BAMClqZW0"
+        title="Github Spark is insane"
+        desc="GitHub Spark helps you transform your ideas into full-stack intelligent apps and publish with a single click."
+      />
+
+      <Card
+        imageSrc="https://media.daily.dev/image/upload/f_auto,q_auto/v1/posts/4f23427091604f46dd498e291f24ac67?_a=AQAEulh"
+        title="Github Spark is insaaskdaso;kde"
+        desc="GitHub Spark helps you transform your ideas into full-stack intelligent apps and publish with a sinasdsadsadgle click."
+      />
+    </>
+  );
+}
+
+export default App;
