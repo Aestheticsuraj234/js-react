a8c1558fb6d471655b1035ba65a15cb0a2f0b0cf
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-31 11:57:29 +0530

    chapter-4

---
+++ Javascript/chapter-4/index.js
@@ -0,0 +1,41 @@
+// function greet(){
+//     console.log("Hello world");
+// }
+
+// greet()
+
+// const greet = function(){
+//     console.log("Hello world");
+// }
+
+// greet()
+
+// * Parameter and Arguments
+
+// parameter ----> function decleration
+// arguments ----> function calling
+
+function greetUsername(username , age){
+    console.log(`Hello👋🏻! Good Morning☀️` , username , age)
+}
+
+greetUsername("Karan" ,21)
+
+
+// *arrow function
+const Add = (a,b)=>( a+b)
+
+// If you have single para then you can follow this syntax
+// const greet = username=>console.log(username)
+console.log(
+Add(12,34)
+)
+
+// greet("Suraj")
+
+// function subs(a,b){
+//     return a-b
+// }
+
+// console.log(-22)
+
