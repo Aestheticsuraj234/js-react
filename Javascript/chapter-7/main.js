152e9f0925088d51867c120474b58d1a738e8040
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-31 17:24:23 +0530

    added

--- Javascript/chapter-7/main.js
+++ Javascript/chapter-7/main.js
@@ -1,16 +1,38 @@
-// let str1 = "Hello world this is 'suraj' ";
-// str1[0] = "lajsdlisa"
-// console.log(str1)
-// let str2 =  'Hello world this is "suraj" ';
+// // let str1 = "Hello";
+// let str2 = 'World';
 
-// let str3 = `My Followers ${str1}`
+// let str3 = `Hi ${str1} ${str2}`;
+// console.log(str3);
 
-// // console.log(str3)
+// let name = "❤️‍🔥🍃";
 
-// let name = "🐦‍🔥";
+// console.log(name.length)
 
-// console.log(name.length);
+// let word = "JavaScript";
+// console.log(word.charAt(4)); // S
 
-let username = "learn javascript";
 
-console.log(username.replace("javascript" , "python"));
+// let str = "JavaScript";
+// console.log(str.slice(0, 4));   // "Java"
+// console.log(str.slice(-6));    // "Script"
+
+
+// let name = "Code";
+// console.log(name.toUpperCase()); // "CODE"
+// console.log(name.toLowerCase()); // "code"
+
+// let messy = "  Hello World  ";
+// console.log(messy.trim()); // "Hello World"
+
+let title = "Learn JavaScript";
+console.log(title.includes("Java"));    // true
+console.log(title.includes("java"));    // false
+
+
+// let txt = "banana";
+// console.log(txt.indexOf("a"));      // 1
+// console.log(txt.lastIndexOf("a"));  // 5
+
+
+let msg = "I love JavaScript";
+console.log(msg.replace("love", "like")); // "I like JavaScript"
